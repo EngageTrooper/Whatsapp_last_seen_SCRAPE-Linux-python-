bd1314ac1521a934192d69fdb9b7e8fd5bbefa88
Shivam Singh <88527987+EngageTrooper@users.noreply.github.com>
2021-08-16 17:56:09 +0530

    Add files via upload

---
+++ Recommended_Update.js
@@ -0,0 +1,5 @@
+//If the code Does not work as intended
+// Do the following changes
+// Line 13 in wls.js
+
+lastSeenData = document.querySelector("._3Id9P"textContent
---
+++ WhatsTrack.js
@@ -0,0 +1,91 @@
+// FIRST OPEN WHATSAPP WEB AND THE INBOX YOU WANT TO TRACK
+// INJECT THESE LINES OF CODE INTO CONSOLE
+// ONLINE OR LAST SEEN TRACK SECTION
+
+
+let lastSeenData;
+//document.getElementById
+
+
+let isOnline = function(){
+	
+	
+	try{
+		lastSeenData = document.querySelector("._2YPr_.i0jNr").textContent
+		
+		//console.log(lastSeenData)
+		
+		if(lastSeenData=="online"){
+			return true;
+		}	
+		return false;
+	}
+	catch(e){
+		//console.log(e.message)
+		return false;
+	}			
+	
+}
+
+let timeout;
+let onlineTrace=[];
+let timeInTimeOut=[];
+
+
+
+
+let onlineBool=[];
+
+let showOnlineTrace = function(){
+	let data="";
+	for(let i=0;i<onlineTrace.length;i++){
+		let s='[From '+onlineTrace[i][0]+' To '+onlineTrace[i][1] +']'
+		
+		data+=s+'\n';
+
+	}
+	// return data;
+	console.log(data);
+
+}
+
+
+let checkLastSeen = function(){
+
+	let status = isOnline()
+	
+	onlineBool.push(status)
+	let lastOnlineBoolIndex = onlineBool.length-1
+
+	let t = new Date() 
+	
+	//console.log(onlineBool[lastOnlineBoolIndex-1])
+
+	if(status){
+		
+
+		time = t.getDate()+'-'+(t.getMonth()+1)+'-'+t.getFullYear()+' '+t.getHours()+':'+t.getMinutes()+':'+t.getSeconds()
+
+		if(lastOnlineBoolIndex==0){
+			timeInTimeOut.push(time)
+		}
+
+		else if(onlineBool[lastOnlineBoolIndex-1]==false){
+			timeInTimeOut.push(time)
+		}
+
+	}
+	else{
+		if(onlineBool[lastOnlineBoolIndex-1]==true){
+			timeInTimeOut.push(time)	
+			onlineTrace.push(timeInTimeOut)
+			timeInTimeOut=[]
+		}		
+	} 
+
+	timeout = setTimeout(checkLastSeen,1000)
+}
+
+
+
+checkLastSeen();
